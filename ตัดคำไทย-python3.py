529168d1490366f126cb42dc56a754c461f3325d
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2016-12-18 15:58:38 +0700

    Create ตัดคำไทย-python3.py

---
+++ ตัดคำไทย-python3.py
@@ -0,0 +1,30 @@
+# เดติดโค้ดต้นฉบับคุณ Chad Humphries
+# Fork โดย นาย วรรณพงษ์  ภัททิยไพบูลย์
+# https://python3.wannaphong.com/2015/03/ตัดคำไทยในภาษา-python.html
+import PyICU
+
+def isThai(chr):
+    cVal = ord(chr)
+    if(cVal >= 3584 and cVal <= 3711):
+        return True
+    return False
+def warp(txt):
+    #print(txt)
+    bd = PyICU.BreakIterator.createWordInstance(PyICU.Locale("th"))
+    bd.setText(txt)
+    lastPos = bd.first()
+    retTxt = ""
+    try:
+        while(1):
+            currentPos = next(bd)
+            retTxt += txt[lastPos:currentPos]
+            #เฉพาะภาษาไทยเท่านั้น
+            if(isThai(txt[currentPos-1])):
+                if(currentPos < len(txt)):
+                    if(isThai(txt[currentPos])):
+                        #คั่นคำที่แบ่ง
+                        retTxt += "|"
+            lastPos = currentPos
+    except StopIteration:
+        pass
+        #retTxt = retTxt[:
